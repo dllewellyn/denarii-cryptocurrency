b1520aa351be6440e4edeb1aece063b1b529a5d5
danielllewellyn <danieln.llewellyn@gmail.com>
2019-05-22 14:22:52 +0100

    Installation location added to readme

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,18 @@
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import com.jfrog.bintray.gradle.BintrayExtension
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.3.21"
     id("com.jfrog.bintray") version "1.8.3"
     `maven-publish`
-    id("com.github.johnrengelman.shadow") version "2.0.2"
 }
 
 val publicationName = "CoinbaseAPI"
 val g = "com.dllewellyn.coinbaseAPI"
-val v ="1.1"
+val v ="1.2"
 group = g
 version = v
 
 val ktor_version = "1.2.0-rc2"
-var scarlet_version = "0.1.7"
 
 repositories {
     mavenCentral()
@@ -35,16 +31,11 @@ dependencies {
 }
 
 
-val shadowJar: ShadowJar by tasks
-shadowJar.apply {
-    baseName = publicationName
-}
-
 publishing {
     publications {
         register(publicationName, MavenPublication::class) {
             artifactId = publicationName
-            artifact(shadowJar)
+            from(components["java"])
         }
     }
 }
@@ -68,18 +59,4 @@ bintray {
             name = v
         }
     })
-}
-
-tasks {
-    withType(GradleBuild::class.java) {
-        dependsOn(shadowJar)
-    }
-
-    withType<GenerateMavenPom> {
-        destination = file("$buildDir/libs/${shadowJar.archiveName}.pom")
-    }
-
-    withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "1.8"
-    }
 }
