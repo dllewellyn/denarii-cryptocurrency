b3c0d2b0506300b0b74e1a239a7d9f1fcb1c1b46
Daniel Llewellyn <danieln.llewellyn@gmail.com>
2019-09-27 16:57:19 +0100

    Punting SQL Delight repository up to the server

---
+++ core/src/main/kotlin/com/dllewellyn/coinbaseapi/callbacks/GenericCallback.kt
@@ -0,0 +1,14 @@
+package com.dllewellyn.coinbaseapi.callbacks
+
+interface GenericCallback<T> {
+    fun onSuccess(value: T)
+    fun onError(exception: Throwable)
+}
+
+class SimpleCallback<T>(private val success: (T) -> Unit, private val failure: (Throwable) -> Unit) :
+    GenericCallback<T> {
+    override fun onSuccess(value: T) = success(value)
+
+    override fun onError(exception: Throwable) = failure(exception)
+
+}
