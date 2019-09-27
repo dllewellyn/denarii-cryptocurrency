5a328c9c488479e74a81d8b99f33dc81d1db7ba1
Daniel Llewellyn <danieln.llewellyn@gmail.com>
2019-09-27 14:42:10 +0100

    Adding a coinbase API

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,12 +5,20 @@ buildscript {
         jcenter()
         google()
     }
+    dependencies {
+        classpath("com.android.tools.build:gradle:3.4.2")
+        classpath("com.squareup.sqldelight:gradle-plugin:1.2.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.50")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:1.3.50")
+    }
+
 }
 
 plugins {
     kotlin("jvm") version "1.3.50"
     id("com.jfrog.bintray") version "1.8.3"
     `maven-publish`
+
 }
 
 repositories {
