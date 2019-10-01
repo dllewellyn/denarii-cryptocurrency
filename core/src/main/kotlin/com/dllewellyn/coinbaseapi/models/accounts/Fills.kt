f08f8f78f133889e2973df42ffe0496a0bacd157
dllewellyn <danieln.llewellyn@gmail.com>
2019-10-01 13:27:03 +0100

    Adding fills

--- core/src/main/kotlin/com/dllewellyn/coinbaseapi/models/accounts/Fills.kt
+++ core/src/main/kotlin/com/dllewellyn/coinbaseapi/models/accounts/Fills.kt
@@ -1,21 +1,23 @@
 package com.dllewellyn.coinbaseapi.models.accounts
 
+import com.dllewellyn.coinbaseapi.models.BuyOrSell
 import com.dllewellyn.coinbaseapi.models.currency.CurrencyPair
 import java.lang.IllegalArgumentException
 import java.math.BigDecimal
 
-enum class LIQUIDITY(val v : String) {
+enum class LIQUIDITY(val v: String) {
     TAKER("T"),
     MAKER("M");
 
     companion object {
-        fun fromString(v : String) = when(v) {
+        fun fromString(v: String) = when (v) {
             "T" -> TAKER
             "M" -> MAKER
             else -> throw IllegalArgumentException()
         }
     }
 }
+
 data class Fills(
     val tradeId: Int,
     val productId: CurrencyPair,
@@ -23,8 +25,8 @@ data class Fills(
     val size: BigDecimal,
     val orderId: String,
     val createdAt: String,
-    val liquidity: String, //The liquidity field indicates if the fill was the result of a liquidity provider or liquidity taker. M indicates Maker and T indicates Taker.
-    val fee: String,
+    val liquidity: LIQUIDITY, //The liquidity field indicates if the fill was the result of a liquidity provider or liquidity taker. M indicates Maker and T indicates Taker.
+    val fee: BigDecimal,
     val settled: Boolean,
-    val side: String
+    val side: BuyOrSell
 )
