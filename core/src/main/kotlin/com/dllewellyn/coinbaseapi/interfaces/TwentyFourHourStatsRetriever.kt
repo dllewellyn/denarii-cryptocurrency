aa959e974a583a6ae2b2e0286dd1e3809054bd6c
dllewellyn <danieln.llewellyn@gmail.com>
2019-10-03 10:00:19 +0100

    Initial move over to having a pro library

--- core/src/main/kotlin/com/dllewellyn/coinbaseapi/interfaces/TwentyFourHourStatsRetriever.kt
+++
@@ -1,9 +0,0 @@
-package com.dllewellyn.coinbaseapi.interfaces
-
-import com.dllewellyn.coinbaseapi.models.currency.CurrencyPair
-import com.dllewellyn.coinbaseapi.models.marketinfo.TwentyFourHourStats
-import com.dllewellyn.coinbaseapi.repositories.ReadOnlyPostRepository
-
-abstract class TwentyFourHourStatsRetriever : ReadOnlyPostRepository<CurrencyPair, TwentyFourHourStats> {
-    suspend fun get24HourStats(cryptoCurrency: CurrencyPair) = retrieveData(cryptoCurrency)
-}
