bbc773573c9730ba60ad729d577dde3658e088eb
dllewellyn <danieln.llewellyn@gmail.com>
2019-10-03 10:00:19 +0100

    Initial move over to having a pro library

--- core/src/main/kotlin/com/dllewellyn/coinbaseapi/interfaces/CurrencyList.kt
+++
@@ -1,8 +0,0 @@
-package com.dllewellyn.coinbaseapi.interfaces
-
-import com.dllewellyn.coinbaseapi.models.currency.SupportedCurrency
-import com.dllewellyn.coinbaseapi.repositories.ReadOnlyRepository
-
-abstract class CurrencyList : ReadOnlyRepository<List<SupportedCurrency>> {
-    suspend fun getCurrencyList() = retrieveData()
-}
