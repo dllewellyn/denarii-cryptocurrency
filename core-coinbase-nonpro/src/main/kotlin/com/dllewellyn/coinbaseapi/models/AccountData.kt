49364533e77358254a306f773753a4ec2f84da99
danielllewellyn <danieln.llewellyn@gmail.com>
2019-10-06 15:51:29 +0100

    :)

--- core-coinbase-nonpro/src/main/kotlin/com/dllewellyn/coinbaseapi/models/AccountData.kt
+++ core-coinbase-nonpro/src/main/kotlin/com/dllewellyn/coinbaseapi/models/AccountData.kt
@@ -5,14 +5,16 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class AccountData(
     val balance: BalanceApi,
-    val created_at: String,
-    val currency: String,
+    val created_at: String? = null,
+    val currency: CurrencyApi,
     val id: String,
     val name: String,
     val primary: Boolean,
-    val ready: Boolean?=null,
+    val allow_deposits : Boolean,
+    val allow_withdrawals : Boolean,
+    val ready: Boolean? = null,
     val resource: String,
     val resource_path: String,
     val type: String,
-    val updated_at: String?=null
+    val updated_at: String? = null
 )
