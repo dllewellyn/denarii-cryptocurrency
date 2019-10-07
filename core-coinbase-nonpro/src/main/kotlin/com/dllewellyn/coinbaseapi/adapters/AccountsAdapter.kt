d7965cbd652cbac87e01e315eacdae36f7e21f19
Daniel Llewellyn <danieln.llewellyn@gmail.com>
2019-10-07 08:46:59 +0100

    Working

--- core-coinbase-nonpro/src/main/kotlin/com/dllewellyn/coinbaseapi/adapters/AccountsAdapter.kt
+++ core-coinbase-nonpro/src/main/kotlin/com/dllewellyn/coinbaseapi/adapters/AccountsAdapter.kt
@@ -5,7 +5,7 @@ import com.dllewellyn.coinbaseapi.interfaces.Accounts
 import com.dllewellyn.coinbaseapi.models.UserAccountApi
 import io.ktor.client.request.get
 
-class AccountsAdapter(private val client: InternalHttpClient) : Accounts {
+class AccountsAdapter(private val client: InternalHttpClient) : Accounts() {
     override suspend fun getAllAccounts() =
         client.httpClient.get<UserAccountApi>(client.url("accounts"))
 }
--- core-coinbase-nonpro/src/main/kotlin/com/dllewellyn/coinbaseapi/interfaces/Accounts.kt
+++ core-coinbase-nonpro/src/main/kotlin/com/dllewellyn/coinbaseapi/interfaces/Accounts.kt
@@ -1,7 +1,16 @@
 package com.dllewellyn.coinbaseapi.interfaces
 
 import com.dllewellyn.coinbaseapi.models.UserAccountApi
+import com.dllewellyn.coinbaseapi.repositories.ReadOnlyRepository
+import java.math.BigDecimal
 
-interface Accounts {
-    suspend fun getAllAccounts() : UserAccountApi
+abstract class Accounts : ReadOnlyRepository<UserAccountApi> {
+    override suspend fun retrieveData() = getAllAccounts()
+    abstract suspend fun getAllAccounts(): UserAccountApi
+    suspend fun getAllNonEmptyAccounts() =
+        with(getAllAccounts()) {
+            copy(data = data.filter {
+                BigDecimal(it.balance.amount) > BigDecimal(0)
+            })
+        }
 }
