bbf9de133a468784b6280a224acdb9afd5a86e79
dllewellyn <danieln.llewellyn@gmail.com>
2019-10-11 07:45:59 +0100

    Renaming to buy sell price

--- core-coinbase-nonpro/src/main/kotlin/com/dllewellyn/coinbaseapi/adapters/ExchangeRateRetriverAdapter.kt
+++ core-coinbase-nonpro/src/main/kotlin/com/dllewellyn/coinbaseapi/adapters/ExchangeRateRetriverAdapter.kt
@@ -3,8 +3,11 @@ package com.dllewellyn.coinbaseapi.adapters
 import com.dllewellyn.coinbaseapi.http.InternalHttpClient
 import com.dllewellyn.coinbaseapi.interfaces.ExchangeRateRetriver
 import com.dllewellyn.coinbaseapi.models.ApiCurrencyRates
+import com.dllewellyn.coinbaseapi.models.BuyOrSell
 import com.dllewellyn.coinbaseapi.models.ExchangeRates
 import com.dllewellyn.coinbaseapi.models.currency.CryptoCurrency
+import com.dllewellyn.coinbaseapi.models.currency.SupportedCurrency
+import com.dllewellyn.coinbaseapi.models.marketinfo.BuySellPrice
 import io.ktor.client.request.get
 import java.math.BigDecimal
 
@@ -12,12 +15,15 @@ import java.math.BigDecimal
 class ExchangeRateRetriverAdapter(private val httpClient: InternalHttpClient) :
     ExchangeRateRetriver() {
 
-    override suspend fun retrieveData(arg: CryptoCurrency) =
+    override suspend fun retrieveData(arg: CryptoCurrency)  =
         httpClient.httpClient.get<ApiCurrencyRates>("${httpClient.url("exchange-rates")}?=${arg.str}")
             .let {
                 ExchangeRates(
                     arg,
                     it.data.rates.mapValues { rate -> BigDecimal(rate.value) })
             }
+            .map.entries.map {
+                BuySellPrice(BuyOrSell.SPOT, it.value, SupportedCurrency(it.key), "coinbase")
+        }
 
 }
