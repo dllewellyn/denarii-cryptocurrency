03178c180d337f7c97c925b4ecfb7640b266667f
Daniel Llewellyn <daniel.llewellyn@xdesign.com>
2019-10-12 11:39:27 +0100

    Working utils

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ buildscript {
         classpath("com.squareup.sqldelight:gradle-plugin:1.2.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.50")
         classpath("org.jetbrains.kotlin:kotlin-serialization:1.3.50")
+        classpath("com.google.cloud.tools:appengine-gradle-plugin:2.0.0-rc4")
     }
 
 }
