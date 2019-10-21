e94a36129b572b3bb86662dfb03272bf5d48820d
dllewellyn <danieln.llewellyn@gmail.com>
2019-10-21 08:37:20 +0100

    Working utils

--- crypto-utils-api/src/main/kotlin/crypto/utils/api/Application.kt
+++ crypto-utils-api/src/main/kotlin/crypto/utils/api/Application.kt
@@ -1,14 +1,32 @@
 package crypto.utils.api
 
+import crypto.utils.api.auth.FirebaseUtil
 import io.micronaut.runtime.Micronaut
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.info.License
+import io.swagger.v3.oas.annotations.servers.Server
 
+@OpenAPIDefinition(
+    info = Info(
+        title = "Denarii - Cryptocurrency utils",
+        version = "1.0",
+        description = "",
+        license = License(name = "Apache 2.0", url = "https://coinbaseliveexchange.appspot.com")
+    )
+)
+@Server(
+    description = "Live server",
+    url = "https://coinbaseliveexchange.appspot.com"
+    )
 object Application {
 
     @JvmStatic
     fun main(args: Array<String>) {
+        FirebaseUtil.initialise()
         Micronaut.build()
-                .packages("crypto.utils.api")
-                .mainClass(Application.javaClass)
-                .start()
+            .packages("crypto.utils.api")
+            .mainClass(Application.javaClass)
+            .start()
     }
 }
