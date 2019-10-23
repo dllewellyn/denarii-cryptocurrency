a774aa20225f189e2c56ad2983ec499134b39f96
Daniel Llewellyn <danieln.llewellyn@gmail.com>
2019-10-23 21:18:00 +0100

    Adding ability to synchronise accounts into firebae

--- core-coinbase-nonpro/src/main/kotlin/com/dllewellyn/coinbaseapi/models/OauthProvider.kt
+++ core-coinbase-nonpro/src/main/kotlin/com/dllewellyn/coinbaseapi/models/OauthProvider.kt
@@ -2,8 +2,8 @@ package com.dllewellyn.coinbaseapi.models
 
 data class OauthProvider(
     val access_token: String,
-    val created_at: Int,
-    val expires_in: Int,
+    val created_at: Double,
+    val expires_in: Double,
     val refresh_token: String,
     val scope: String,
     val token_type: String
--- crypto-utils-api/src/main/kotlin/crypto/utils/api/firebase/FirebaseUserStorage.kt
+++ crypto-utils-api/src/main/kotlin/crypto/utils/api/firebase/FirebaseUserStorage.kt
@@ -1,41 +1,86 @@
 package crypto.utils.api.firebase
 
+import com.dllewellyn.coinbaseapi.models.Account
+import com.dllewellyn.coinbaseapi.models.OauthProvider
+import com.dllewellyn.coinbaseapi.repositories.ReadOnlyRepository
+import com.dllewellyn.coinbaseapi.repositories.ReadOnlyRepositoryArgument
 import com.dllewellyn.coinbaseapi.repositories.WriteRepository
+import com.dllewellyn.coinbaseapi.repositories.WriteRepositoryArgument
 import com.google.firebase.cloud.FirestoreClient
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import crypto.utils.api.auth.dataForUser
+import crypto.utils.api.auth.docForUser
+import crypto.utils.api.oauth.OauthModel
 import crypto.utils.api.oauth.OauthWrapper
+import java.lang.IllegalArgumentException
 import javax.inject.Singleton
 
 @Singleton
-class FirebaseUserStorage : WriteRepository<OauthWrapper> {
+class FirebaseAccountsStorage : WriteRepositoryArgument<String, List<Account>> {
+    private val firestore = FirestoreClient.getFirestore()
 
+    override suspend fun write(value: List<Account>, arg: String) {
+        with(firestore.docForUser(arg)) {
+            firestore.dataForUser(arg)
+                ?.let {
+                    it.set("accounts", value.convert())
+                    update(it)
+                } ?: create(mutableMapOf("accounts" to value.convert()))
+        }
+    }
+
+}
+
+fun <I, O> I.convert(): O {
     val gson = Gson()
+    val json = gson.toJson(this)
+    return gson.fromJson(json, object : TypeToken<O>() {}.type)
+}
+
+@Singleton
+class FirebaseUserStorage : WriteRepository<OauthWrapper>, ReadOnlyRepositoryArgument<String, OauthProvider> {
+
+
+    private val firestore = FirestoreClient.getFirestore()
 
     fun <T> T.serializeToMap(): Map<String, Any> {
         return convert()
     }
 
-    private inline fun <I, reified O> I.convert(): O {
-        val json = gson.toJson(this)
-        return gson.fromJson(json, object : TypeToken<O>() {}.type)
+    fun <T> Map<String, Any>.toDataClass(): T {
+        return convert()
     }
+
 
     override suspend fun write(value: OauthWrapper) {
-        val firestore = FirestoreClient.getFirestore()
-        val document = firestore
-            .collection("users")
-            .document(value.user)
-        with(document) {
-            get()
-                .get()
-                .data
+        with(firestore.docForUser(value.user)) {
+            firestore.dataForUser(value.user)
                 ?.let {
                     it.set("coinbase", value.oauth.convert())
-                    document.update(it)
-                } ?: document.create(mutableMapOf("coinbase" to value.oauth.convert()))
+                    update(it)
+                } ?: create(mutableMapOf("coinbase" to value.oauth.convert()))
         }
-
-
     }
+
+    override suspend fun retrieveData(arg: String): OauthProvider {
+        val data = firestore
+            .collection("users")
+            .document(arg)
+            .get()
+            .get()
+
+        val coinbaseData = data?.data?.get("coinbase") as? Map<String, Any>
+        return coinbaseData?.let {
+            OauthProvider(
+                coinbaseData["access_token"] as String,
+                coinbaseData["created_at"] as Double,
+                coinbaseData["expires_in"] as Double,
+                coinbaseData["refresh_token"] as String,
+                coinbaseData["scope"] as String,
+                coinbaseData["token_type"] as String
+            )
+        } ?: throw IllegalArgumentException()
+    }
+
 }
